a85535607c7f3de9958acb51b0ec555dc8b40df3
tpluscode <tpluscode@users.noreply.github.com>
2022-06-01 19:49:47 +0200

    refactor: separate univoca and meta dimension

--- apps/api/lib/SharedDimension.js
+++ apps/api/lib/SharedDimension.js
@@ -1,5 +1,6 @@
 import { rdf, schema } from '@tpluscode/rdf-ns-builders'
-import { univoca } from './ns.js'
+import httpError from 'http-errors'
+import { univoca, meta } from './ns.js'
 
 export function injectTermsLink(req, pointer) {
   pointer.any().has(rdf.type, schema.DefinedTermSet).forEach((termSet) => {
@@ -9,15 +10,25 @@ export function injectTermsLink(req, pointer) {
   })
 }
 
+const createdDimensionTypes = [univoca.NewDimension, univoca.ImportedDimension]
+
 export function prepareCreated(req, pointer) {
-  pointer.addOut(rdf.type, univoca('SharedDimension.created'))
+  // TODO https://github.com/hypermedia-app/creta/issues/388
+  if (req.method === 'POST' && !pointer.has(rdf.type, createdDimensionTypes).terms.length) {
+    throw new httpError.BadRequest('Missing expected RDF type')
+  }
+
+  pointer
+    .out(univoca.dimension)
+    .addOut(rdf.type, [meta.SharedDimension, schema.DefinedTermSet])
+
+  // TODO: when univoca:dimension is blank node, turn it into a URI
 }
 
 export function removeGeneratedProperties({ after }) {
   after.deleteOut([
-    univoca.createAs,
     univoca.terms,
     univoca.export,
   ])
-  after.deleteOut(rdf.type, univoca('SharedDimension.created'))
+  after.deleteOut(rdf.type, createdDimensionTypes)
 }
--- apps/api/lib/ns.js
+++ apps/api/lib/ns.js
@@ -1,3 +1,4 @@
 import namespace from '@rdfjs/namespace'
 
 export const univoca = namespace('https://cube.link/univoca/')
+export const meta = namespace('https://cube.link/meta/')
